800491191f6f7308df989a7b136510d73d82d043
“Jen <jenstewart@gmail.com>
2021-07-12 16:07:50 +0100

    Added getting older functionality & test

--- test/virtual-pet.test.js
+++ test/virtual-pet.test.js
@@ -1,17 +1,29 @@
 const Pet = require("../src/virtual-pet");
 
 describe('constructor', () => {
-    // let newPet;
-    // beforeEach(() => {
-    //     newPet = new Pet("Tako");
-    // });
-
     it("returns an object", () => {
       expect(new Pet("Tako")).toBeInstanceOf(Object);
     });
+
     const kitty = new Pet("Tako");
     it("gives the pet a name", () => {
         expect(kitty.name).toBe("Tako");
+    });
+
+    it('has a initial age of 0', () => {
+        const kitty = new Pet("Tako");
+    
+        expect(kitty.age).toEqual(0);
+      });
+  });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const kitty = new Pet("Tako");
+  
+      kitty.growUp();
+  
+      expect(kitty.age).toEqual(1);
     });
   });
   
