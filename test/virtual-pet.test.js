bb4c609adb74d41a4c27bb1d147fd4558dc89b22
“Jen <jenstewart@gmail.com>
2021-07-12 16:33:33 +0100

    Added additional hunger & fitness functionality & tests to growUp

--- test/virtual-pet.test.js
+++ test/virtual-pet.test.js
@@ -1,29 +1,46 @@
 const Pet = require("../src/virtual-pet");
 
-describe('constructor', () => {
+describe("constructor", () => {
+    let kitty;
+    beforeEach(() => {
+        kitty = new Pet("Tako");
+    });
+
     it("returns an object", () => {
       expect(new Pet("Tako")).toBeInstanceOf(Object);
     });
 
-    const kitty = new Pet("Tako");
     it("gives the pet a name", () => {
         expect(kitty.name).toBe("Tako");
     });
 
-    it('has a initial age of 0', () => {
-        const kitty = new Pet("Tako");
-    
-        expect(kitty.age).toEqual(0);
-      });
+    it("has a initial age of 0", () => {
+        expect(kitty.age).toBe(0);
+    });
+
+    it("has a initial hunger of 0", () => {
+        expect(kitty.hunger).toBe(0);
+    });
+
+    it("has a initial fitness of 10", () => {
+        expect(kitty.fitness).toBe(10);
+    });
   });
 
   describe('growUp', () => {
-    it('increments the age by 1', () => {
-      const kitty = new Pet("Tako");
-  
-      kitty.growUp();
-  
-      expect(kitty.age).toEqual(1);
+    const kitty = new Pet("Tako");
+    kitty.growUp();
+
+    it("increments the age by 1", () => {
+        expect(kitty.age).toBe(1);
+    });
+
+    it("increases hunger by 5 when age increments by 1", () => {
+        expect(kitty.hunger).toBe(5);
+    });
+
+    it("decreases fitness by 3 when age increments by 1", () => {
+        expect(kitty.fitness).toBe(7);
     });
   });
   
