d20777608a569b833985bbc224fa84c5d3160662
“Jen <jenstewart@gmail.com>
2021-07-14 21:17:37 +0100

    Refactored virtual-pet

--- src/virtual-pet.js
+++ src/virtual-pet.js
@@ -1,5 +1,6 @@
 const MAXIMUM_FITNESS = 10;
 const MINIMUM_HUNGER = 0;
+const MAXIMUM_AGE = 30;
 const HUNGRY = 5;
 const UNFIT = 3;
 
@@ -13,55 +14,55 @@ function Pet(name) {
 
 Pet.prototype = {
     get isAlive() {
-      return this.age < 30 && this.hunger < 10 && this.fitness > 0;
-    }
-};
+      return this.age < MAXIMUM_AGE && this.hunger < 10 && this.fitness > 0;
+    },
 
-Pet.prototype.growUp = function() {
-    if (!this.isAlive) {
-        throw new Error("Your pet is no longer alive :(");
-    } else { this.age += 1;
-    this.hunger += 5;
-    this.fitness -= 3;
-    };
-};
-  
-Pet.prototype.play = function() {
-    if (!this.isAlive) {
-        throw new Error("Your pet is no longer alive :(");
-    } if ((this.fitness + 4) <= 10 ) {
-      this.fitness += 4;
-    } else {
-      this.fitness = 10;
-    }
-};
+    growUp() {
+        if (!this.isAlive) {
+            throw new Error("Your pet is no longer alive :(");
+        } else { this.age += 1;
+        this.hunger += HUNGRY;
+        this.fitness -= UNFIT;
+        };
+    },
 
-Pet.prototype.feed = function() {
-    if (!this.isAlive) {
-        throw new Error("Your pet is no longer alive :(");
-    } if ((this.hunger - 3) > MINIMUM_HUNGER ) {
-      this.hunger -= 3;
-    } else {
-      this.hunger = MINIMUM_HUNGER;
-    }
-};
+    play() {
+        if (!this.isAlive) {
+            throw new Error("Your pet is no longer alive :(");
+        } if ((this.fitness + 4) <= MAXIMUM_FITNESS ) {
+          this.fitness += 4;
+        } else {
+          this.fitness = MAXIMUM_FITNESS;
+        }
+    },
 
-Pet.prototype.checkUp = function() {
-    if (!this.isAlive) {
-        throw new Error("Your pet is no longer alive :(");
-    } if(this.fitness <= UNFIT && this.hunger >= HUNGRY) {
-        return "I want to play AND I'm hungry!"
-    } if(this.fitness <= UNFIT) {
-        return "I want to play!"
-    } if(this.hunger >= HUNGRY) {
-        return "I'm hungry!"
-    } else {
-        return "I feel great!"
-    }
-};
+    feed() {
+        if (!this.isAlive) {
+            throw new Error("Your pet is no longer alive :(");
+        } if ((this.hunger - 3) > MINIMUM_HUNGER ) {
+          this.hunger -= 3;
+        } else {
+          this.hunger = MINIMUM_HUNGER;
+        }
+    },
 
-Pet.prototype.adoptChild = function(pet) {
-    this.children.push(pet);
+    checkUp() {
+        if (!this.isAlive) {
+            throw new Error("Your pet is no longer alive :(");
+        } if(this.fitness <= UNFIT && this.hunger >= HUNGRY) {
+            return "I want to play AND I'm hungry!"
+        } if(this.fitness <= UNFIT) {
+            return "I want to play!"
+        } if(this.hunger >= HUNGRY) {
+            return "I'm hungry!"
+        } else {
+            return "I feel great!"
+        }
+    },
+
+    adoptChild(pet) {
+        this.children.push(pet);
+    }
 };
 
 module.exports = Pet;
