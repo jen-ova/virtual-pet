16684f3c65f9ff5be63ddf803654996ce891a176
“Jen <jenstewart@gmail.com>
2021-07-14 21:24:22 +0100

    Test file final

--- test/virtual-pet.test.js
+++ test/virtual-pet.test.js
@@ -1,9 +1,9 @@
 const Pet = require("../src/virtual-pet");
 
 describe("constructor", () => {
-    let kitty;
+    let pet;
     beforeEach(() => {
-        kitty = new Pet("Tako");
+        pet = new Pet("Tako");
     });
 
     it("returns an object", () => {
@@ -11,154 +11,154 @@ describe("constructor", () => {
     });
 
     it("gives the pet a name", () => {
-        expect(kitty.name).toBe("Tako");
+        expect(pet.name).toBe("Tako");
     });
 
     it("has a initial age of 0", () => {
-        expect(kitty.age).toBe(0);
+        expect(pet.age).toBe(0);
     });
 
     it("has a initial hunger of 0", () => {
-        expect(kitty.hunger).toBe(0);
+        expect(pet.hunger).toBe(0);
     });
 
     it("has a initial fitness of 10", () => {
-        expect(kitty.fitness).toBe(10);
+        expect(pet.fitness).toBe(10);
     });
   });
 
 describe('growUp', () => {
-    const kitty = new Pet("Tako");
-    kitty.growUp();
+    const pet = new Pet("Tako");
+    pet.growUp();
 
     it("increments the age by 1", () => {
-        expect(kitty.age).toBe(1);
+        expect(pet.age).toBe(1);
     });
 
     it("increases hunger by 5 when age increments by 1", () => {
-        expect(kitty.hunger).toBe(5);
+        expect(pet.hunger).toBe(5);
     });
 
     it("decreases fitness by 3 when age increments by 1", () => {
-        expect(kitty.fitness).toBe(7);
+        expect(pet.fitness).toBe(7);
     });
   });
 
 describe("play", () => {
-    const kitty = new Pet("Tako");
+    const pet = new Pet("Tako");
     it("increases fitness by 4", () => {
-      kitty.fitness = 4;
-      kitty.play();
+      pet.fitness = 4;
+      pet.play();
       
-      expect(kitty.fitness).toBe(8);
+      expect(pet.fitness).toBe(8);
     });
 
     it("increases fitness to a maximum of 10", () => {
-        kitty.fitness = 8;
-        kitty.play();
+        pet.fitness = 8;
+        pet.play();
         
-        expect(kitty.fitness).toBe(10);
+        expect(pet.fitness).toBe(10);
       });
   });
 
 describe("feed", () => {
-    const kitty = new Pet("Tako");
+    const pet = new Pet("Tako");
     it("decreases hunger by 3", () => {
-      kitty.hunger = 4;
-      kitty.feed();
+      pet.hunger = 4;
+      pet.feed();
       
-      expect(kitty.hunger).toBe(1);
+      expect(pet.hunger).toBe(1);
     });
 
     it("doesn't allow hunger to go below 0", () => {
-        kitty.hunger = 2;
-        kitty.feed();
+        pet.hunger = 2;
+        pet.feed();
         
-        expect(kitty.hunger).toBe(0);
+        expect(pet.hunger).toBe(0);
       });
   });
   
 describe("checkUp", () => {
-    const kitty = new Pet("Tako");
+    const pet = new Pet("Tako");
 
     it("checks if the pet is unfit", () => {
-        kitty.fitness = 2;
-        kitty.hunger = 3;
+        pet.fitness = 2;
+        pet.hunger = 3;
         
-        expect(kitty.checkUp()).toBe("I want to play!");
+        expect(pet.checkUp()).toBe("I want to play!");
     });
   
     it("checks if the pet is hungry", () => {
-        kitty.fitness = 5;
-        kitty.hunger = 6;
+        pet.fitness = 5;
+        pet.hunger = 6;
           
-        expect(kitty.checkUp()).toBe("I'm hungry!");
+        expect(pet.checkUp()).toBe("I'm hungry!");
     });
 
     it("checks if the pet is unfit and hungry", () => {
-        kitty.fitness = 3;
-        kitty.hunger = 7;
+        pet.fitness = 3;
+        pet.hunger = 7;
         
-        expect(kitty.checkUp()).toBe("I want to play AND I'm hungry!");
+        expect(pet.checkUp()).toBe("I want to play AND I'm hungry!");
     });
 
     it("checks if the pet is fit and full", () => {
-        kitty.fitness = 6;
-        kitty.hunger = 2;
+        pet.fitness = 6;
+        pet.hunger = 2;
         
-        expect(kitty.checkUp()).toBe("I feel great!");
+        expect(pet.checkUp()).toBe("I feel great!");
     });
   });
 
 
   describe("isAlive", () => {
-    const kitty = new Pet("Tako");
+    const pet = new Pet("Tako");
 
     it("tells us if the pet is alive", () => {
-        kitty.fitness = 6;
-        kitty.hunger = 2;
-        kitty.age = 3;
-        expect(kitty.isAlive).toBe(true);
+        pet.fitness = 6;
+        pet.hunger = 2;
+        pet.age = 3;
+        expect(pet.isAlive).toBe(true);
     });
 
     it("tells us if the pet has died from being unfit", () => {
-        kitty.fitness = 0; 
-        expect(kitty.isAlive).toBe(false);
+        pet.fitness = 0; 
+        expect(pet.isAlive).toBe(false);
     });
 
     it("tells us if the pet has died from hunger", () => {
-        kitty.hunger = 10;
-        expect(kitty.isAlive).toBe(false);
+        pet.hunger = 10;
+        expect(pet.isAlive).toBe(false);
     });
 
     it("tells us if the pet has died from old age", () => {
-        kitty.age = 30;
-        expect(kitty.isAlive).toBe(false);
+        pet.age = 30;
+        expect(pet.isAlive).toBe(false);
     });
 
   });
 
   describe("feed", () => {
-    const kitty = new Pet("Tako");
+    const pet = new Pet("Tako");
 
     it("checks for a growUp error", () => {
-        kitty.age = 30;
-        expect(() => kitty.growUp()).toThrow("Your pet is no longer alive :(");
+        pet.age = 30;
+        expect(() => pet.growUp()).toThrow("Your pet is no longer alive :(");
     });
 
     it("checks for a play error", () => {
-        kitty.age = 30;
-        expect(() => kitty.play()).toThrow("Your pet is no longer alive :(");
+        pet.age = 30;
+        expect(() => pet.play()).toThrow("Your pet is no longer alive :(");
     });
   
     it("checks for a feed error", () => {
-        kitty.age = 30;
-        expect(() => kitty.feed()).toThrow("Your pet is no longer alive :(");
+        pet.age = 30;
+        expect(() => pet.feed()).toThrow("Your pet is no longer alive :(");
     });
 
     it("checks for a checkup error", () => {
-        kitty.age = 30;
-        expect(() => kitty.checkUp()).toThrow("Your pet is no longer alive :(");
+        pet.age = 30;
+        expect(() => pet.checkUp()).toThrow("Your pet is no longer alive :(");
     });
 });
 
